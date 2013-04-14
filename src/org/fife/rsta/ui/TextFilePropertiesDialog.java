a19ba8d1355ee10bac2fc1c8112f15f6b76c3153
bobbylight <robert@fifesoft.com>
2013-04-14 05:17:00 +0000

    RSTA: Update idea.xml sample theme.  RSTAUI: Remove write to stdout.

--- src/org/fife/rsta/ui/TextFilePropertiesDialog.java
+++ src/org/fife/rsta/ui/TextFilePropertiesDialog.java
@@ -428,7 +428,6 @@ public class TextFilePropertiesDialog extends EscapableDialog
 			SwingUtilities.invokeLater(new Runnable() {
 				public void run() {
 					filePathField.requestFocusInWindow();
-					System.out.println(filePathField.getText() + "!!");
 					filePathField.selectAll();
 				}
 			});
